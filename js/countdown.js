927f520b3373d6d0a6e88575660b0154c413af1b
Rafael Zegarra Huerta <rafaelzegarra5555@gmail.com>
2024-04-11 10:47:47 -0500

    cambiar fecha de entrega

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,7 @@
 simplyCountdown('#cuenta', {
 	year: 2024, // required
 	month: 4, // required
-	day: 5, // required
+	day: 15, // required
 	hours: 12, // Default is 0 [0-23] integer
 	minutes: 31, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
